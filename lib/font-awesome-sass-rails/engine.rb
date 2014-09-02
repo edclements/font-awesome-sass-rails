e775787bc479d4dc2745803779d463ed99d8ba7f
Ed Clements <ed@bookingbug.com>
2014-09-02 10:26:20 +0100

    Include fonts in precompile

--- lib/font-awesome-sass-rails/engine.rb
+++ lib/font-awesome-sass-rails/engine.rb
@@ -2,6 +2,10 @@ module FontAwesome
   module Sass
     module Rails
       class Engine < ::Rails::Engine
+        initializer 'fontawesome-sass-rails.assets.precompile' do |app|
+          app.config.assets.precompile << %r(FontAwesome\.otf)
+          app.config.assets.precompile << %r(fontawesome-webfont\.(?:eot|svg|ttf|woff)$)
+        end
       end
     end
   end
